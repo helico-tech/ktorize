db13d3901a1054728a90ee0abd6a99c2455b8271
Arjan van Wieringen <avanwieringen@MacBook-Pro-8.local>
2024-10-28 07:15:52 +0100

    Squashed commit of the following:
    commit 1baaed3a482a9d9ff6d9b2cfbda36d96dd9ba75d
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.local>
    Date:   Mon Oct 28 07:10:16 2024 +0100
    
        WIP
    
    commit 39fc133423e6bed0d0137bde077bcbb7bbfe0726
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.local>
    Date:   Mon Oct 28 06:48:31 2024 +0100
    
        Some DI stuffs
    
    commit 4df44a7a7829243451f10d3e2b326087eef342c3
    Merge: ae0bc11 2a76a74
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 13:38:48 2024 +0100
    
        Merge branch 'main' into buma
    
    commit ae0bc115865c9f590802675db401fcdb99bc6595
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 13:38:01 2024 +0100
    
        Certs
    
    commit 53d5968bc5ec4db7fc449a1610701fe12922f379
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 09:19:27 2024 +0100
    
        Removed ALPN implementation
    
    commit cd03e37f2f35c5ce68e5bb85b9e50f8632988ba9
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 09:15:26 2024 +0100
    
        Working HTTP2
    
    commit b70a4c79de3755c63b33f1e05ae39ccc49576c7f
    Merge: 861eccf 4de353e
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 08:32:23 2024 +0100
    
        Merge branch 'main' into buma
    
    commit 861eccfa12196c99b531f1c6b4c0593a4a14e64a
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 08:28:57 2024 +0100
    
        Cleanup
    
    commit 4edeafb51d2159368812d0d6a392d66ec5286af6
    Merge: 2d65198 d6b599c
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sat Oct 26 18:36:20 2024 +0200
    
        Merge branch 'main' into buma
    
    commit 2d651987dc0c5947fd2b7d00505759f42ed1ab79
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 20 09:41:57 2024 +0200
    
        Remove task
    
    commit 0562314a6c87f26dc91dbda426d929af193650f4
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sat Oct 19 17:36:37 2024 +0200
    
        Added dev tasks
    
    commit f0e8cf6aeb25b3ef7031483ace463cc448518d1e
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sat Oct 19 17:15:29 2024 +0200
    
        Setup

--- examples/simple-server/src/main/kotlin/App.kt
+++ examples/simple-server/src/main/kotlin/App.kt
@@ -1,34 +1,43 @@
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
-import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.html.*
 import nl.helico.ktorize.assetmapper.AssetMapperPlugin
-import nl.helico.ktorize.assetmapper.createMappedAssetRegex
+import nl.helico.ktorize.di.DI
+import nl.helico.ktorize.di.DIModuleRegistry
+import nl.helico.ktorize.di.DIPlugin
 import nl.helico.ktorize.hotwire.stimulus.HotwireStimulusPlugin
 import nl.helico.ktorize.hotwire.stimulus.stimulusController
 import nl.helico.ktorize.hotwire.turbo.HotwireTurboPlugin
-import nl.helico.ktorize.hotwire.turbo.TurboFrame
 import nl.helico.ktorize.html.respondHtml
 import nl.helico.ktorize.importmap.ImportMapPlugin
+import org.kodein.di.bindSingleton
+import org.kodein.di.direct
+import org.kodein.di.instance
+import org.kodein.di.provider
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
+
+        DIModuleRegistry.register("Test") {
+            bindSingleton<String>("test") { "Hello, World!" }
+        }
 
         install(AssetMapperPlugin)
         install(ImportMapPlugin)
         install(HotwireTurboPlugin)
         install(HotwireStimulusPlugin)
+        install(DIPlugin)
 
         routing {
+
+            val greeting = application.DI.direct.instance<String>("test")
 
             get("/") {
                 call.respondHtml {
                     head {
-                        title { +"Hello, World!" }
+                        title { + greeting}
                         link(rel = "stylesheet", href = "/assets/css/styles.css")
                     }
                     body {
--- websites/bumastemra/src/main/kotlin/nl/bumastemra/portal/features/auth/Feature.kt
+++ websites/bumastemra/src/main/kotlin/nl/bumastemra/portal/features/auth/Feature.kt
@@ -1,17 +1,10 @@
 package nl.bumastemra.portal.features.auth
 
+import io.ktor.server.application.*
 import io.ktor.server.routing.*
-import kotlinx.html.body
-import nl.helico.ktorize.html.respondHtml
 
-fun Routing.authFeature(root: String = "/") {
-
-    get("$root/login") {
-        call.respondHtml {
-            body {
-                +"Login"
-            }
-        }
+fun Application.authFeature() {
+    routing {
+        controllers()
     }
-
 }
