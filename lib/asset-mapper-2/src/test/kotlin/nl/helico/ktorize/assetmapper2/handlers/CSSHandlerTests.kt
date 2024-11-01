17ff6c9c76efbc6fb36300b32aa4652f0d373db1
Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
2024-11-01 14:28:54 +0100

    Base test

--- lib/asset-mapper-2/src/test/kotlin/nl/helico/ktorize/assetmapper2/handlers/CSSHandlerTests.kt
+++ lib/asset-mapper-2/src/test/kotlin/nl/helico/ktorize/assetmapper2/handlers/CSSHandlerTests.kt
@@ -6,6 +6,7 @@ import nl.helico.ktorize.assetmapper2.readers.StringAssetReader
 import nl.helico.ktorize.assetmapper2.writers.NullAssetWriter
 import kotlin.io.path.Path
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class CSSHandlerTests {
 
@@ -22,7 +23,10 @@ class CSSHandlerTests {
         val mapper = createMapper(handler,
             "no-imports.css" to Fixtures.CSS.`no imports`
         )
-        val input = mapper.reader.readAsset(Path("no-imports.css"))
+        val input = mapper.reader.readAsset(Path("no-imports.css"))!!
+        val output = handler.handle(input, mapper)
+        assertEquals(Fixtures.CSS.`no imports`.lines(), output.lines)
+        assertEquals("no-imports.c8c1acc1b093fe24b8beb00623cd2501.css", output.path.fileName.toString())
     }
 
     private fun createMapper(handler: CSSHandler, vararg data: Pair<String, String>): AssetMapper {
