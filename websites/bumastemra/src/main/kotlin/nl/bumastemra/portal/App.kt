120763bcae9cf7e9f7da2df6f686dfe16e8624b8
Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
2024-11-03 08:48:42 +0100

    Squashed commit of the following:
    commit 5d896b5044d4ea9e884d907c850418da3a21fdfd
    Merge: f5d9be9 54976d0
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Nov 3 08:46:54 2024 +0100
    
        Merge branch 'main' into buma
    
        # Conflicts:
        #	lib/asset-mapper/src/main/kotlin/nl/helico/ktorize/assetmapper/CSSAssetTransformer.kt
    
    commit f5d9be952025e4fdc2e3c7dac5ef0afa75c03332
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Fri Nov 1 09:12:13 2024 +0100
    
        Wip
    
    commit bc8636c3b1547c042f242dcfaa0a434f5e182809
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.local>
    Date:   Wed Oct 30 08:09:14 2024 +0100
    
        WIP
    
    commit 61fc3523ee539b9dae5dfd89c80dee69643fff15
    Merge: 1baaed3 c50b3b7
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Tue Oct 29 20:02:07 2024 +0100
    
        Merge branch 'main' into buma
    
        # Conflicts:
        #	examples/simple-server/src/main/kotlin/App.kt
        #	lib/html/src/main/kotlin/nl/helico/ktorize/html/ApplicationCall.kt
        #	lib/html/src/main/kotlin/nl/helico/ktorize/html/Html.kt
    
    commit 1baaed3a482a9d9ff6d9b2cfbda36d96dd9ba75d
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.local>
    Date:   Mon Oct 28 07:10:16 2024 +0100
    
        WIP
    
    commit 39fc133423e6bed0d0137bde077bcbb7bbfe0726
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.local>
    Date:   Mon Oct 28 06:48:31 2024 +0100
    
        Some DI stuffs
    
    commit 4df44a7a7829243451f10d3e2b326087eef342c3
    Merge: ae0bc11 2a76a74
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 13:38:48 2024 +0100
    
        Merge branch 'main' into buma
    
    commit ae0bc115865c9f590802675db401fcdb99bc6595
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 13:38:01 2024 +0100
    
        Certs
    
    commit 53d5968bc5ec4db7fc449a1610701fe12922f379
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 09:19:27 2024 +0100
    
        Removed ALPN implementation
    
    commit cd03e37f2f35c5ce68e5bb85b9e50f8632988ba9
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 09:15:26 2024 +0100
    
        Working HTTP2
    
    commit b70a4c79de3755c63b33f1e05ae39ccc49576c7f
    Merge: 861eccf 4de353e
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 08:32:23 2024 +0100
    
        Merge branch 'main' into buma
    
    commit 861eccfa12196c99b531f1c6b4c0593a4a14e64a
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 27 08:28:57 2024 +0100
    
        Cleanup
    
    commit 4edeafb51d2159368812d0d6a392d66ec5286af6
    Merge: 2d65198 d6b599c
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sat Oct 26 18:36:20 2024 +0200
    
        Merge branch 'main' into buma
    
    commit 2d651987dc0c5947fd2b7d00505759f42ed1ab79
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sun Oct 20 09:41:57 2024 +0200
    
        Remove task
    
    commit 0562314a6c87f26dc91dbda426d929af193650f4
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sat Oct 19 17:36:37 2024 +0200
    
        Added dev tasks
    
    commit f0e8cf6aeb25b3ef7031483ace463cc448518d1e
    Author: Arjan van Wieringen <avanwieringen@MacBook-Pro-8.home>
    Date:   Sat Oct 19 17:15:29 2024 +0200
    
        Setup

--- websites/bumastemra/src/main/kotlin/nl/bumastemra/portal/App.kt
+++ websites/bumastemra/src/main/kotlin/nl/bumastemra/portal/App.kt
@@ -3,8 +3,15 @@ package nl.bumastemra.portal
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
 import nl.bumastemra.portal.features.auth.authFeature
+import nl.helico.ktorize.assetmapper.AssetMapperPlugin
+import nl.helico.ktorize.hotwire.turbo.HotwireTurboPlugin
+import nl.helico.ktorize.importmap.ImportMapPlugin
 
 fun Application.root() {
+  install(AssetMapperPlugin)
+  install(ImportMapPlugin)
+  install(HotwireTurboPlugin)
+
   authFeature()
 }
 
--- websites/bumastemra/src/main/kotlin/nl/bumastemra/portal/features/auth/Controllers.kt
+++ websites/bumastemra/src/main/kotlin/nl/bumastemra/portal/features/auth/Controllers.kt
@@ -1,12 +1,15 @@
 package nl.bumastemra.portal.features.auth
 
+import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import nl.helico.ktorize.html.respondHtml
 
-fun Routing.controllers() {
-    route("/auth") {
-        get("/") {
-            call.respondHtml(view = LoginPage)
+fun Application.controllers() {
+    routing {
+        route("/auth") {
+            get("") {
+                call.respondHtml(view = LoginPage)
+            }
         }
     }
 }
