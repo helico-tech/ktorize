57a812789bdf8179879aad679ead0ff2780d2ef1
Arjan van Wieringen <avanwieringen@MacBook-Pro-801.local>
2024-11-20 08:23:35 +0100

    Main page

--- websites/bumastemra/src/main/kotlin/nl/bumastemra/portal/features/dashboard/View.kt
+++ websites/bumastemra/src/main/kotlin/nl/bumastemra/portal/features/dashboard/View.kt
@@ -1,6 +1,6 @@
 package nl.bumastemra.portal.features.dashboard
 
-import kotlinx.html.button
+import kotlinx.html.a
 import kotlinx.html.classes
 import kotlinx.html.div
 import kotlinx.html.h1
@@ -18,12 +18,36 @@ val DashboardView = PortalLayout(title = "Dashboard") {
         + "In jouw persoonlijke omgeving meld je nieuw werk aan, zie je wat je muziek opbrengt en krijg je unieke inzichten in jouw muziekdata. Log in om te starten."
     }
 
-    section("login") {
+    section("kt-component--panel panel") {
         h2 { + "Log in om verder te gaan" }
         p { + "Heb je al een BumaStemra account? Log in en ga direct naar je dashboard" }
         div("buttons") {
-            button(classes = "kt-component--button") { + "Inloggen" }
-            button(classes = "kt-component--button") { + "Wachtwoord vergeten" }
+            a(classes = "kt-component--button") { + "Inloggen" }
+            a(classes = "kt-component--button") { + "Wachtwoord vergeten" }
+        }
+    }
+
+    section("panels") {
+        section("onboarding kt-component--panel panel") {
+            h2 { + "Wel lid, geen account?" }
+            p { + "Maak dan snel een account aan, zodat je toegang krijgt tot jouw persoonlijke omgeving. Het is binnen een paar stappen geregeld." }
+            div("buttons") {
+                a(classes = "kt-component--button") { +"Account aanmaken" }
+            }
+        }
+        section("membership-registration kt-component--panel panel") {
+            h2 { + "Nog geen lid?" }
+            p { + "Dan kun je ook nog geen account aanmaken, geen werken aanmelden en dus ook niets verdienen aan je auteursrecht. Kijk hier of lid worden iets voor jou is." }
+            div("buttons") {
+                a(classes = "kt-component--button") { +"Word lid" }
+            }
+        }
+        section("live kt-component--panel panel") {
+            h2 { + "Setlijsten aanleveren als niet-lid" }
+            p { + "Klik hieronder om setlijsten aan te leveren als je geen lid bent bij BumaStemra." }
+            div("buttons") {
+                a(classes = "kt-component--button") { +"Naar setlijsten" }
+            }
         }
     }
 }
