f316a29687da1fd13a115e4f591748e1f08b6219
Arjan van Wieringen <avanwieringen@MBP-van-Arjan-2.home>
2025-01-03 14:35:32 +0100

    Website

--- websites/helico-tech/src/main/kotlin/nl/helico/website/Layout.kt
+++ websites/helico-tech/src/main/kotlin/nl/helico/website/Layout.kt
@@ -6,13 +6,17 @@ import nl.helico.ktorize.hotwire.turbo.enableViewTransition
 import nl.helico.ktorize.hotwire.turbo.turboPrefetch
 import nl.helico.ktorize.hotwire.turbo.turboRefreshMethod
 import nl.helico.ktorize.html.HTMLView
+import java.time.Year
+import java.util.Date
 
 fun Layout(
     lang: String = "nl",
     title: String = "Helico Tech",
+    main: MAIN.() -> Unit = {}
 ): HTMLView = {
 
     this.lang = lang
+    this.classes = setOf("ht-layout")
 
     head {
         title(content = title)
@@ -25,7 +29,7 @@ fun Layout(
         }
         link(
             rel = "stylesheet",
-            href = "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+            href = "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
         )
 
         turboPrefetch(false)
@@ -35,15 +39,57 @@ fun Layout(
 
     body {
         header {
+            a(href = "/") {
+                img(src = "/assets/img/logo.svg", alt = "Helico Tech")
+            }
 
+            nav {
+                a(href = "/") {
+                    +"Home"
+                }
+
+                a(href = "/about") {
+                    +"About"
+                }
+
+                a(href = "/contact") {
+                    +"Contact"
+                }
+            }
         }
 
         main {
-
+            main()
         }
 
         footer {
+            section {
+                p {
+                    + "KvK: 92080936"
+                }
 
+                p {
+                    + "BTW: NL865877221B01"
+                }
+
+                p {
+                    + "IBAN: NL65ABNA0129012882"
+                }
+
+                p {
+                    span {
+                        + "Email: "
+                    }
+
+                    a(href = "mailto:info@helico-tech.nl") {
+                        + "info@helico-tech.nl"
+                    }
+                }
+
+                p {
+                    +"© ${Year.now().value} Helico Tech B.V."
+                }
+            }
         }
     }
 }
--- websites/helico-tech/src/main/kotlin/nl/helico/website/Routes.kt
+++ websites/helico-tech/src/main/kotlin/nl/helico/website/Routes.kt
@@ -1,15 +1,117 @@
 package nl.helico.website
 
 import io.ktor.server.application.Application
-import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
+import kotlinx.html.FlowContent
+import kotlinx.html.a
+import kotlinx.html.article
+import kotlinx.html.classes
+import kotlinx.html.h1
+import kotlinx.html.h2
+import kotlinx.html.img
+import kotlinx.html.li
+import kotlinx.html.section
+import kotlinx.html.span
+import kotlinx.html.title
+import kotlinx.html.ul
 import nl.helico.ktorize.html.respondHtml
 
 fun Application.routes() {
     routing {
         get("/") {
-            call.respondHtml(view = Layout())
+            call.respondHtml(view = Layout {
+                classes += "ht-home"
+
+                section("title") {
+                    h1 {
+                        span("gradient") {
+                            +"H"
+                        }
+                        span {
+                            +"elico"
+                        }
+                        span("gradient") {
+                            +" T"
+                        }
+                        span {
+                            +"ech"
+                        }
+                    }
+                }
+
+                section("subtitle") {
+                    h2 {
+                        +"Suspendisse vestibulum, orci ac vulputate laoreet, justo massa maximus leo, ut condimentum diam enim in lectus. Sed facilisis at eros nec efficitur. Proin vehicula quam orci, vel maximus dui dictum non. Sed semper leo ac pellentesque eleifend. Cras molestie scelerisque ex, quis auctor eros malesuada nec. "
+                    }
+                }
+
+                section("services") {
+                    Article(
+                        title = "Software Development",
+                        className = "software-development",
+                        imageSrc = "/assets/img/home/development.png",
+                        link = "/services/software-development",
+                        items = listOf(
+                            "Web Development",
+                            "Mobile Development",
+                            "Desktop Development"
+                        )
+                    )
+
+                    Article(
+                        title = "Lead Developer",
+                        className = "lead-developer",
+                        imageSrc = "/assets/img/home/developer.png",
+                        link = "/services/lead-developer",
+                        items = listOf(
+                            "Team Lead",
+                            "Architect",
+                            "Mentor"
+                        )
+                    )
+
+                    Article(
+                        title = "Consultancy",
+                        className = "consultancy",
+                        imageSrc = "/assets/img/home/consultancy.png",
+                        link = "/services/consultancy",
+                        items = listOf(
+                            "Code Review",
+                            "Architecture",
+                            "Mentoring"
+                        )
+                    )
+                }
+            })
+        }
+    }
+}
+
+internal fun FlowContent.Article(
+    title: String,
+    className: String,
+    imageSrc: String,
+    link: String,
+    items: List<String>
+) {
+    article(className) {
+        h1 {
+            +title
+        }
+
+        section {
+            img(src = imageSrc, alt = title)
+            ul {
+                items.forEach {
+                    li {
+                        +it
+                    }
+                }
+            }
+            a(href = link) {
+                +"Read more"
+            }
         }
     }
 }
